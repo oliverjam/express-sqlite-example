ab5771ab536d2646fa38692229e421f5285d5b87
oliverjam <oliverjamesphillips@gmail.com>
2022-09-09 15:54:04 +0100

    Explain why we use prepared statements

--- src/model/tasks.js
+++ src/model/tasks.js
@@ -2,6 +2,12 @@ const db = require("../database/db.js");
 
 module.exports = { listTasks };
 
+/**
+ * Create a "prepared statement" _outside_ the function
+ * Created once when the server starts and re-used for each request.
+ * This is faster.
+ * See: https://github.com/WiseLibs/better-sqlite3/issues/793
+ */
 const select_tasks = db.prepare(/*sql*/ `
   SELECT content FROM tasks
 `);
