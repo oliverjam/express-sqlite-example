ef382725425c67655ad02ddaf1d6ff41fd9f45d7
oliverjam <oliverjamesphillips@gmail.com>
2023-06-02 10:25:49 +0100

    Only use actual SQLite types
    BOOLEAN and DATETIME aren't real so we shouldn't lie about them.

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS tasks (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   content TEXT,
   -- SQLite doesn't have proper booleans, so we use 0 or 1
-  complete BOOLEAN DEFAULT 0 CHECK (complete IN (0, 1)),
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  complete INTEGER DEFAULT 0 CHECK (complete IN (0, 1)),
+  created_at TEXT DEFAULT CURRENT_TIMESTAMP
 );
 
 COMMIT;
