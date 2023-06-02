ec01bee039e4b06c5701aafab795d3d689c7f453
oliverjam <oliverjamesphillips@gmail.com>
2023-06-02 10:26:08 +0100

    Always default to making SQLite tables STRICT

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS tasks (
   -- SQLite doesn't have proper booleans, so we use 0 or 1
   complete INTEGER DEFAULT 0 CHECK (complete IN (0, 1)),
   created_at TEXT DEFAULT CURRENT_TIMESTAMP
-);
+) STRICT;
 
 COMMIT;
